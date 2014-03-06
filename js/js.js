b05345852c9d2015ac76bfd2a9fe2a0799dc429c
mazikwyry <mazik.wyry@gmail.com>
2014-03-06 23:59:46 +0100

    photo gallery

--- js/js.js
+++ js/js.js
@@ -33,6 +33,7 @@
         return false;
     });
 
+
     function parallaxScroll(){
         var scrolled = $(window).scrollTop();
         var section_height = $(window).height();
@@ -53,3 +54,59 @@
     });
 
    });
+
+$(function(){
+    var x=0,
+        y=0,
+        rate=0,
+        maxspeed=20;
+    var backdrop = $('.photo-box');
+
+    $('.direction').mousemove(function(e){
+        var $this = $(this);
+        var left = $this.is('.left_d');
+
+        if (left){
+            var w = $this.width();
+            rate = 1 -((e.pageX - $(this).offset().left + 1)/w);
+        }
+        else{
+            var w = $this.width();
+            rate = -((e.pageX - $(this).offset().left + 1)/w);
+        }
+    });
+
+    $('.direction').hover(
+        function(){
+            var scroller = setInterval( moveBackdrop, 10 );
+            $(this).data('scroller', scroller);
+        },
+        function(){
+            var scroller = $(this).data('scroller');
+            clearInterval( scroller );
+        }
+    );   
+
+    $('.photo-cat').click(function(event) {
+      var active = $('.active-cat');
+      active.removeClass('active-cat');
+      active.fadeOut('slow');
+      x=0;
+      rate=0;
+      $(".direction").each(function(index, el) {
+        var scroller = $(this).data('scroller');
+        clearInterval( scroller );
+      });
+      $('.photo-box').animate({"left":"0px"});
+      $("#"+$(this).data("rel")).addClass('active-cat').fadeIn('slow');
+    });
+
+    function moveBackdrop(){
+        x += maxspeed * rate;
+        max = $(".active-cat").width() - $('.photos').width() + $('.photos').offset().left
+        x > 0 ? x=0 : true;
+        x < -max ? x=-max : true;
+        var newpos = x+'px';
+        backdrop.css('left',newpos);
+    }
+});
